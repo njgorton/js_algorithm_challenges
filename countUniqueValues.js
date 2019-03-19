6d4d9005e13d95da37f5f2dcf874c17087547f9d
njgorton <gorton.nj@gmail.com>
2019-03-19 17:04:42 -0600

    countUniqueValues challenge

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,3 +1,20 @@
 function countUniqueValues(arr) {
+    let first = 0;
+    let second = 1;
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[first] == arr[second]) {
+            second++;
+        } else {
+            first++;
+            arr.splice(first, 1, arr[second]);
+            second++;
+        }
+    }
+
+    console.log(first)
 }
+
+// Input a SORTED array of numbers and this function will log the amount of unique values:
+countUniqueValues([1, 2, 3, 3, 5, 9, 9]);     //should log 5
+
