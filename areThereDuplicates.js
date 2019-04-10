ab9e1e82ad90f2e16b832e1b4262ce8f0843060f
njgorton <gorton.nj@gmail.com>
2019-04-09 22:15:17 -0600

    areThereDuplicates challenge

---
+++ areThereDuplicates.js
@@ -0,0 +1,25 @@
+/*
+Implement a function which accepts a variable number of arguments,
+and checks whether there are any duplicates among the arguments passed in.
+Solve it with either the Frequency Counter pattern, or the Multiple Pointers pattern.
+
+Restrictions:
+Time  - O(n)
+Space - O(n)
+
+Bonus:
+Time  - O(n log n)
+Space - O(1)
+*/
+
+function areThereDuplicates() {
+
+}
+
+areThereDuplicates(1, 2, 3)
+
+
+// Examples:
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a') // true
